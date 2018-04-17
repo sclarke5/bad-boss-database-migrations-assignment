293b1c7c5a7eb37897df130c3f11cf11229d1b26
Sclarke5 <sclarke5555@gmail.com>
2018-04-17 14:23:09 -0400

    renames party_guests table to widgets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180417175210) do
+ActiveRecord::Schema.define(version: 20180417181655) do
 
   create_table "locations", force: :cascade do |t|
     t.string  "address"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20180417175210) do
     t.integer "quantity"
   end
 
-  create_table "party_guests", force: :cascade do |t|
+  create_table "widgets", force: :cascade do |t|
     t.string  "first_name"
     t.string  "last_name"
     t.string  "dietary_restrictions"
